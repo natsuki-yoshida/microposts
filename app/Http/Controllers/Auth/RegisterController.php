101113889138087a688b044ec8256a83c46b2934
natsuki-yoshida <ec2-user@ip-172-31-34-30.us-east-2.compute.internal>
2018-06-08 01:32:37 +0000

    user registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -21,45 +21,22 @@ class RegisterController extends Controller
     */
 
     use RegistersUsers;
+   protected $redirectTo = '/';
 
-    /**
-     * Where to redirect users after registration.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('guest');
     }
 
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
+            'name' => 'required|string|max:191',
+            'email' => 'required|string|email|max:191|unique:users',
             'password' => 'required|string|min:6|confirmed',
         ]);
     }
 
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @param  array  $data
-     * @return \App\User
-     */
     protected function create(array $data)
     {
         return User::create([
@@ -69,3 +46,15 @@ class RegisterController extends Controller
         ]);
     }
 }
+    /**
+     * Where to redirect users after registration.
+     *
+     * @var string
+     */
+
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
